4c6c5d59adb805d4f523635b1f8a0caea773599a
beckpalmx77 <beckpalmx@gmail.com>
2023-05-24 21:11:06 +0700

    update

---
+++ cronjob/delete_data_sale_sac.php
@@ -0,0 +1,36 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(~0);
+
+include("../config/connect_sqlserver.php");
+include("../config/connect_db.php");
+
+
+$sql_sqlsvr = "SELECT DOCINFO.DI_KEY,DOCINFO.DI_REF,DOCINFO.DI_ACTIVE FROM DOCINFO WHERE DOCINFO.DI_REF.DI_ACTIVE = 1";
+
+$res = "";
+
+
+$stmt_sqlsvr = $conn_sqlsvr->prepare($sql_sqlsvr);
+$stmt_sqlsvr->execute();
+
+$return_arr = array();
+
+while ($result_sqlsvr = $stmt_sqlsvr->fetch(PDO::FETCH_ASSOC)) {
+
+    $sql_delete = "DELETE FROM ims_product_sale_sac 
+                WHERE DI_KEY = :DI_KEY  AND DI_REF  = :DI_RE";
+
+    $query = $conn->prepare($sql_delete);
+    $query->bindParam(':DI_KEY', $result_sqlsvr["DI_KEY"], PDO::PARAM_STR);
+    $query->bindParam(':DI_REF', $result_sqlsvr["DI_REF"], PDO::PARAM_STR);
+
+    $query->execute();
+
+    $delete_data .= $result_sqlsvr["DI_KEY"] . ":" . $result_sqlsvr["DI_REF"] . " |- "  . "\n\r";
+
+    echo " Delete DATA " . $delete_data;
+
+
+}

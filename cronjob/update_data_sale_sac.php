151529e0c9e2356e4213e1ee371d6c0414668fa9
beckpalmx77 <beckpalmx@gmail.com>
2023-05-30 19:57:00 +0700

    Update update_data_sale_sac.php

--- cronjob/update_data_sale_sac.php
+++ cronjob/update_data_sale_sac.php
@@ -27,7 +27,7 @@ echo "\n\r" . date("Y/m/d", strtotime("yesterday"));
 
 $query_daily_cond_ext = " AND (DOCTYPE.DT_DOCCODE in ('2','DS02','IS01','IS02','IV01','IV3')) ";
 
-$query_year = " AND DI_KEY >= " . "1119985";
+$query_year = " AND DI_KEY >= " . "1119995";
 //$query_year = " AND DI_DATE BETWEEN '" . date("Y/m/d", strtotime("yesterday")) . "' AND '" . date("Y/m/d") . "'";
 //$query_year = " AND DI_DATE BETWEEN '2019/12/23' AND '2019/12/23'";
 //$query_year = " AND DI_DATE BETWEEN '2022/05/15' AND '" . date("Y/m/d") . "'";

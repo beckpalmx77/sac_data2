8cd7e50440826baaeab927404388243dc48cc100
beckpalmx77 <beckpalmx@gmail.com>
2023-06-02 06:10:04 +0700

    Update update_data_sale_sac.php

--- cronjob/update_data_sale_sac.php
+++ cronjob/update_data_sale_sac.php
@@ -37,8 +37,8 @@ $query_daily_cond_ext = " AND (DOCTYPE.DT_DOCCODE in ('2','DS02','IS01','IS02','
 
 
 //$query_year = " AND DI_DATE BETWEEN '" . date("Y/m/d", strtotime("yesterday")) . "' AND '" . date("Y/m/d") . "'";
-$query_year = " AND DI_DATE BETWEEN '2018/12/27' AND '2018/12/27'";
-//$query_year = " AND DI_DATE BETWEEN '2022/05/15' AND '" . date("Y/m/d") . "'";
+$query_year = " AND DI_DATE BETWEEN '2018/05/22' AND '2018/05/31'";
+//$query_year = " AND DI_DATE BETWEEN '2022/05/22' AND '" . date("Y/m/d") . "'";
 
 //$query_year = " AND DI_DATE BETWEEN '2022/08/21' AND '" . date("Y/m/d") . "'";
 

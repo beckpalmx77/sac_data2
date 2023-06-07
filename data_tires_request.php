7b5bda2191acdb416e3863aa930f910a0f11e6dc
beckpalmx77 <beckpalmx@gmail.com>
2023-06-07 20:21:05 +0700

    Update data_tires_request.php

--- data_tires_request.php
+++ data_tires_request.php
@@ -351,7 +351,7 @@ if (strlen($_SESSION['alogin']) == "") {
 
             $("#selTires").select2({
                 ajax: {
-                    url: "model/getg_tires_master_ajaxfile.php",
+                    url: "model/get_tires_master_ajaxfile.php",
                     type: "post",
                     dataType: 'json',
                     delay: 250,

b2e751a23c9467b0f64f411ace7887b60333f578
beckpalmx77 <beckpalmx@gmail.com>
2023-06-13 20:31:59 +0700

    up

--- tests/msg/Test_PG_DB.php
+++ tests/msg/Test_PG_DB.php
@@ -2,9 +2,10 @@
 
 
 $host = 'sac.cckwqocv7kfy.ap-southeast-1.rds.amazonaws.com';
-$db = 'MADB_PRODUCT_2564';
-$username = 'sac';
-$password = "l;ovvF9h8kiN";
+$db = 'sac';
+$username = 'sacdbm';
+$password = "Sac123456";
+
 
 $dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
 
@@ -14,9 +15,16 @@ try {
 
     // display a message if connected to the PostgreSQL successfully
     if ($conn) {
-        echo "Connected to the <strong>$db . ' ON HOST ' . $host  </strong> database successfully!";
+        echo "Connected to the <strong>$db . ' ON HOST ' . $host . ' User Name = ' . $username </strong> database successfully!";
     }
 } catch (PDOException $e) {
     // report error message
     echo $e->getMessage();
 }
+
+/*
+$stmt = $conn->query("SELECT * FROM SC_DOCINFO ORDER BY DI_REF DESC LIMIT 10  ");
+while ($row = $stmt->fetch()) {
+    echo $row['DI_REF']."<br />\n";
+}
+*/

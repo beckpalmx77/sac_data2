06b2f95f2306df19e984bc6b27742f677f7230aa
beckpalmx77 <beckpalmx@gmail.com>
2023-07-05 11:09:04 +0700

    up

--- tests/mq_receive.php
+++ tests/mq_receive.php
@@ -1,6 +1,8 @@
 <?php
-include '../config_pg/connect_pg_db.php';
+date_default_timezone_set("Asia/Bangkok");
+include('../config/db_value.inc');
 include '../config/config_rabbit.inc';
+include '../util/send_message.php';
 require_once '../vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
@@ -8,28 +10,64 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 $connection = new AMQPStreamConnection($rabbitmqHost, $rabbitmqPort, $rabbitmqUser, $rabbitmqPass);
 $channel = $connection->channel();
 
-$channel->queue_declare($rabbitmqQueue, false, false, false, false);
 
-echo "Start = " . $rabbitmqQueue . "\n\r";
+echo 'Waiting for messages. To exit, press CTRL+C', "\n";
 
-echo " [*] Waiting for messages. To exit press CTRL+C\n\r";
-
+// Callback function สำหรับรับข้อมูลจาก RabbitMQ
 $callback = function ($msg) {
-    global $conn;
 
-    $data = $msg->body ;
-    $String_Sql = " SELECT code_id FROM ims_sac_orders WHERE msg_status = 'N' AND code_id = " . $data  . " | " ;
+    try
+    {
+        $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";port=" .DB_PORT,DB_USER, DB_PASS
+            ,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $e)
+    {
+        echo "Error: " . $e->getMessage();
+        exit("Error: " . $e->getMessage());
+    }
 
-    echo "Data = " . $String_Sql;
+    $data = $msg->body;
 
-    echo ' [x] Received ', $msg->body, "\n";
+    // ตัวอย่างการแทรกข้อมูลลงในตารางชื่อ users
+    $sql = "UPDATE ims_sac_orders set msg_status = 'Y' WHERE code_id = " . $data ;
+    $stmt = $conn->prepare($sql);
+
+    try {
+        $stmt->execute();
+        echo 'Update data into MySQL: ' . $data . "\n";
+
+        $sql_find = "SELECT date,customer_code,customer_name FROM ims_sac_orders WHERE code_id = " . $data ;
+
+        $row = $conn->query($sql_find)->fetch();
+        if (!empty($row["1"])) {
+            $date = $row["0"];
+            $customer_code = $row["1"];
+            $customer_name = $row["2"];
+        }
+
+
+        $sToken = "fEdAZErH6afcT2QEZBZ8J17bz3QpBrYCZUYyK3v40ob";
+        $sMessage = "มีรายการสั่งซื้อเข้า เลขที่เอกสาร = " . $data . " " . $date . " " . $customer_code . " " . $customer_name ;
+        echo $sMessage ;
+        sendLineNotify($sMessage,$sToken);
+
+    } catch (PDOException $e) {
+        echo 'Error inserting data: ' . $e->getMessage() . "\n";
+    }
 };
 
+// รับข้อมูลจากคิว RabbitMQ
 $channel->basic_consume($rabbitmqQueue, '', false, true, false, false, $callback);
 
-while ($channel->is_open()) {
+while ($channel->is_consuming()) {
     $channel->wait();
 }
 
+// ปิดการเชื่อมต่อ
 $channel->close();
 $connection->close();
+
+
+
--- tests/pg_test.php
+++ tests/pg_test.php
@@ -1,15 +1,19 @@
 <?php
 include '../config_pg/connect_pg_db.php';
 include '../config/connect_db.php';
+include '../config/config_rabbit.inc';
 require_once '../vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-    $current_date = date("Y-m-d");
+    //$current_date = date("Y-m-d");
+    $current_date = "2023-07-04";
 
     echo $current_date . "\n\r" ;
 
-    $sql_pg = "SELECT * FROM sac_orders WHERE date >= '" . $current_date . "'";
+    $sql_pg = "SELECT sac_orders.*,sac_customers.code,sac_customers.name,sac_customers.owner FROM sac_orders
+    LEFT JOIN sac_customers ON sac_customers.id = sac_orders.customer_id  
+    WHERE date >= '" . $current_date . "'";
 
     echo $sql_pg . "\n\r" ;
 
@@ -26,12 +30,15 @@ use PhpAmqpLib\Message\AMQPMessage;
         } else {
             $data = $order['id'];
             echo "Insert id = " . $data . "\n\r" ;
-            $sql = " INSERT INTO ims_sac_orders (code_id,date,customer_id,address,contract_name,contract_phone) 
-            VALUE (:code_id,:date,:customer_id,:address,:contract_name,:contract_phone) " ;
+            $sql = " INSERT INTO ims_sac_orders (code_id,date,customer_id,customer_code,customer_name,owner,address,contract_name,contract_phone) 
+            VALUE (:code_id,:date,:customer_id,:customer_code,:customer_name,:owner,:address,:contract_name,:contract_phone) " ;
             $query = $conn->prepare($sql);
             $query->bindParam(':code_id', $order["id"], PDO::PARAM_STR);
             $query->bindParam(':date', $order["date"], PDO::PARAM_STR);
             $query->bindParam(':customer_id', $order["customer_id"], PDO::PARAM_STR);
+            $query->bindParam(':customer_code', $order["code"], PDO::PARAM_STR);
+            $query->bindParam(':customer_name', $order["name"], PDO::PARAM_STR);
+            $query->bindParam(':owner', $order["owner"], PDO::PARAM_STR);
             $query->bindParam(':address', $order["address"], PDO::PARAM_STR);
             $query->bindParam(':contract_name', $order["contract_name"], PDO::PARAM_STR);
             $query->bindParam(':contract_phone', $order["contract_phone"], PDO::PARAM_STR);
@@ -40,13 +47,13 @@ use PhpAmqpLib\Message\AMQPMessage;
             $lastInsertId = $conn->lastInsertId();
 
             if ($lastInsertId) {
-                $connection = new AMQPStreamConnection('192.168.88.8', 5672, 'admin', 'admin');
+                $connection = new AMQPStreamConnection($rabbitmqHost, $rabbitmqPort, $rabbitmqUser, $rabbitmqPass);
                 $channel = $connection->channel();
 
-                $channel->queue_declare('sac_msg', false, false, false, false);
+                $channel->queue_declare($rabbitmqQueue, false, false, false, false);
 
                 $msg = new AMQPMessage($data);
-                $channel->basic_publish($msg, '', 'sac_msg');
+                $channel->basic_publish($msg, '', $rabbitmqQueue);
 
                 echo " [x] Sent 'Send Data'\n\r";
 

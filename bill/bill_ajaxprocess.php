9368993284eaa47e98f2427a94930aa54562d235
beckpalmx77 <beckpalmx@gmail.com>
2023-11-28 13:48:52 +0700

    Update bill_ajaxprocess.php

--- bill/bill_ajaxprocess.php
+++ bill/bill_ajaxprocess.php
@@ -89,11 +89,12 @@ if ($_POST["action"] === 'GET_BILL_DATA') {
     $searchByBillNoteDate = $_POST['searchByBillNoteDate'];
 
 
-    $filename = "D:\\temp_app\\post_data_from_front.txt";
+    //$filename = "D:\\temp_app\\post_data_from_front.txt";
+    $filename = "post_data_from_front.txt";
     $myfile = fopen($filename, "w") or die("Unable to open file!");
     fwrite($myfile, $_POST["action"]);
     fclose($myfile);
-    
+
 
 
 ## Search

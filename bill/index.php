95ce81a38202ccf97754c6a8c384a31aeb56be4c
beckpalmx77 <beckpalmx@gmail.com>
2023-11-28 15:02:32 +0700

    up

--- bill/index.php
+++ bill/index.php
@@ -3,7 +3,14 @@
 /* This will give an error. Note the output
  * above, which is before the header() call */
 
-header('Location: login');
+// header('Location: login');
+
+
+header('Location: http://171.100.56.194:8999/sac_data2/bill/login');
+
 
 exit;
 ?>
+
+
+

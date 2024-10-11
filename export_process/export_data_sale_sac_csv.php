2d8c0b336054567100e224f379773d42f2ca9bb2
beckpalmx@gmail.com <beckpalmx@gmail.com>
2024-10-11 17:24:25 +0700

    up

--- export_process/export_data_sale_sac_csv.php
+++ export_process/export_data_sale_sac_csv.php
@@ -65,7 +65,7 @@ if (!empty($sku_cat) && $sku_cat!=='-') {
 $order_by = "order by SALE_NAME,STR_TO_DATE(DI_DATE, '%d-%m-%Y') ";
 
 // Create SQL query
-$select_query_sale_sac = "SELECT * FROM ims_data_sale_sac_all sale_sac WHERE sale_sac.SALE_NAME NOT LIKE '%R%' " . $search_Query . $order_by;
+$select_query_sale_sac = "SELECT * FROM ims_data_sale_sac_all sale_sac WHERE AND DI_REF NOT LIKE 'DS03%' AND DI_REF NOT LIKE 'IS02%' " . $search_Query . $order_by;
 /*
 $txt = "sql = " . $select_query_sale_sac;
 $my_file = fopen("exp_sale_param2.txt", "w") or die("Unable to open file!");

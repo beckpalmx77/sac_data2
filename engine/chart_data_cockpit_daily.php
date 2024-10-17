3bcc742e4e5e8e90dc86e3ecc90a3d427617de35
beckpalmx@gmail.com <beckpalmx@gmail.com>
2024-10-17 17:32:35 +0700

    Update chart_data_cockpit_daily.php

--- engine/chart_data_cockpit_daily.php
+++ engine/chart_data_cockpit_daily.php
@@ -12,11 +12,12 @@ $date = date("Y/m/d");
 $sql_get = "
  SELECT SLMN_NAME,sum(CAST(TRD_G_KEYIN AS DECIMAL(10,2))) as  TRD_G_KEYIN
  FROM ims_product_sale_sac 
- WHERE STR_TO_DATE(DI_DATE,'%d/%m/%Y') BETWEEN CAST('" . $date . "' AS DATE) AND CAST('" . $date . "' AS DATE)
- AND SLMN_NAME NOT LIKE '%R%'
+ WHERE STR_TO_DATE(DI_DATE,'%d/%m/%Y') BETWEEN CAST('" . $date . "' AS DATE) AND CAST('" . $date . "' AS DATE) 
  GROUP BY  SLMN_NAME
  ORDER BY SLMN_NAME
 ";
+
+//AND SLMN_NAME NOT LIKE '%R%'
 
 //$myfile = fopen("qry_file1.txt", "w") or die("Unable to open file!");
 //fwrite($myfile, $sql_get);

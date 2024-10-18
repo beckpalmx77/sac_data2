becf053184669e19c9aedf1983b64afe34ea6d3d
beckpalmx777 <beckpalmx@gmail.com>
2024-10-18 22:01:56 +0700

    Update fetch_sales_data.php

--- model/fetch_sales_data.php
+++ model/fetch_sales_data.php
@@ -27,9 +27,19 @@ $sql = "SELECT AR_NAME,
             ORDER BY SUM DESC ";
 
 /*
-$myfile = fopen("param.txt", "w") or die("Unable to open file!");
-fwrite($myfile, "year = " . $year . "| month = " . $month . " | sale = " . $sale . " | " . $sql);
-fclose($myfile);
+
+SELECT AR_NAME,
+SUM(CASE WHEN SKU_CAT = 'LTB' THEN CAST(TRD_AMOUNT_PRICE AS DECIMAL(10, 2)) ELSE 0 END) AS LTB,
+SUM(CASE WHEN SKU_CAT = 'LTR' THEN CAST(TRD_AMOUNT_PRICE AS DECIMAL(10, 2)) ELSE 0 END) AS LTR,
+SUM(CASE WHEN SKU_CAT = 'TBB' THEN CAST(TRD_AMOUNT_PRICE AS DECIMAL(10, 2)) ELSE 0 END) AS TBB,
+SUM(CASE WHEN SKU_CAT = 'TBR' THEN CAST(TRD_AMOUNT_PRICE AS DECIMAL(10, 2)) ELSE 0 END) AS TBR,
+SUM(CASE WHEN SKU_CAT = 'ยางเล็ก' THEN CAST(TRD_AMOUNT_PRICE AS DECIMAL(10, 2)) ELSE 0 END) AS ยางเล็ก,
+SUM(CAST(TRD_AMOUNT_PRICE AS DECIMAL(10, 2))) AS SUM
+FROM ims_data_sale_sac_all
+WHERE DI_YEAR = '2024' AND SKU_CAT IN ('LTB', 'LTR', 'TBB', 'TBR', 'ยางเล็ก')
+GROUP BY AR_NAME
+ORDER BY SUM DESC
+
 */
 
 // ดำเนินการคำสั่ง SQL
